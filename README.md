7dc3f2a79760c08d8756dae5fb5c50bf8afbce8c
Blue <azhanging@qq.com>
2019-04-09 00:46:33 +0800

    add blue-queue

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # blue-queue
+
+队列处理
+
+### BlueQueue
+
+大致的使用预览
+```javascript
+    var blueQueue = new BlueQueue({
+      data:{},
+      methods:{
+        someMethod:function(){
+          //this指向当前队列实例
+        }
+      },
+      enqueued:function(){
+        //this指向当前队列实例
+      },
+      dequeued:function(){
+        //this指向当前队列实例
+      }
+    });
+
+    setTimeout(function () {
+      blueQueue.useMethod('setStatus', [true]);
+    }, 0);
+
+    blueQueue.enqueue(function () {
+      console.log(1);
+    });
+
+    blueQueue.enqueue(function () {
+      console.log(2);
+    });
+
+    blueQueue.enqueue(function () {
+      console.log(3);
+    });
+
+    blueQueue.enqueue(123);
+
+    blueQueue.run();
+```
+
+#### 实例属性
+
+data:实例化带入的data
+
+queue:队列数组
+
+options:默认带入的配置
+
+methods:实例化带入的methods
+
+#### 钩子
+enqueued: 每次enqueue后调用的钩子，this指向队列实例，
+带入的参数为enqueue后的返回值
+
+dequeued: 每次dequeue后调用的钩子，this指向队列实例，
+带入的参数为dequeue的值
+
+ran: 在dequeue run 后执行
+
+
+#### prototype methods
+_init(opts): 初始化方法
+
+enqueue(obj): 加入队列
+
+dequeue(): 离开队列，返回离开队列的obj
+
+isEmpty(): 检查当前队列是否为空
+
+clear(): 清空队列
+
+first(): 获取到第一个队列
+
+last(): 获取到最后一个队列
+
+run(arguments): 执行队列，arguments会被带入到function类型的dequeue中执行
+
+hook(ctx,fn,args[]):执行fn使用
+
+useMethod(methodName,args[]):使用某一个在methods定义的方法
---
+++ src/blue-queue.js
@@ -0,0 +1,90 @@
+/*
+* blue-queue.js v1.0.0
+* (c) 2016-2020 Blue
+* Released under the MIT License.
+* https://github.com/azhanging/blue-queue
+* time:2019-4-9 00:43:15
+*/
+
+function BlueQueue(opts) {
+  this._init(opts);
+}
+
+BlueQueue.prototype = {
+
+  constructor: BlueQueue,
+
+  _init: function (opts) {
+    if (!opts) opts = {};
+    //配置
+    this.options = opts;
+    //队列
+    this.queue = [];
+    //数据
+    this.data = opts.data || {};
+    //方法
+    this.methods = opts.methods || {};
+    //进列后的执行
+    this.enqueued = opts.enqueued;
+    //出列后的执行
+    this.dequeued = opts.dequeued;
+    //允许时钩子
+    this.ran = opts.ran;
+  },
+
+  enqueue: function (obj) {
+    this.hook(this, this.enqueued, [this.queue.push(obj)]);
+  },
+
+  dequeue: function () {
+    var dequeue = this.queue.shift();
+    this.hook(this, this.dequeued, [dequeue]);
+    return dequeue;
+  },
+
+  isEmpty: function () {
+    return this.queue.length === 0;
+  },
+
+  clear: function () {
+    this.queue = [];
+  },
+
+  first: function () {
+    return this.queue[0];
+  },
+
+  last: function () {
+    return this.queue[this.queue.length - 1];
+  },
+
+  run: function () {
+    while (!this.isEmpty()) {
+      var dequeue = this.dequeue();
+      //如果队列项是function，执行
+      if (typeof dequeue === 'function') {
+        this.hook(this, this.ran, [dequeue({
+          queueCtx: this,
+          args: arguments
+        })]);
+      } else {
+        //非function给dequeued执行
+        this.hook(this, this.ran, [dequeue]);
+      }
+    }
+  },
+
+  hook: function (ctx, fn, args) {
+    (typeof fn === 'function') &&
+    fn.apply(ctx, (args instanceof Array) ? args : []);
+  },
+
+  //使用方法
+  useMethod: function (name, args) {
+    this.hook(this, this.methods[name], args || []);
+  }
+};
+
+module.exports = BlueQueue;
+
+
